1fedfe5fe495771f6210b78bec0fedfeb4437741
pocket-devil <93219960+pocket-devil@users.noreply.github.com>
2022-09-13 16:14:21 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,24 @@
-print("Hello world!")
-print("Bye")
-print("Bye")
+import pytest
+
+#фу-ция возведения в квадрат
+def square(n: int) -> int:
+    return n * n
+
+
+
+#параметры для теста "Возведения в квадрат"
+@pytest.mark.parametrize(
+    ('input_x', 'expected'),
+    (
+            (1, 1),
+            (-1, 1),
+            (2, 4),
+            (-2, 4),
+            (3, 0)
+    ),
+)
+
+
+#проверим, какие из заданных значений удовлетворят условия
+def test_square(input_x, expected):
+    assert square(input_x) == expected
